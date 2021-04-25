60e61e864b18af2a3e1186dca603107f2553a419
NBender8668 <61422838+NBender8668@users.noreply.github.com>
2021-04-25 15:52:32 -0600

    Wrap Fixed

--- Assets/Scripts/Common/Utilites.cs
+++ Assets/Scripts/Common/Utilites.cs
@@ -6,10 +6,11 @@ public static class Utilites
 {
     public static Vector2 Wrap(Vector2 point, Vector2 min, Vector2 max)
     {
-        if (point.x > max.x)
-        {
-            point.x = min.x;
-        }
+        if (point.x > max.x) point.x = min.x; 
+        if (point.y > max.y) point.y = min.y; 
+        if (point.x < min.x) point.x = max.x; 
+        if (point.y < min.y) point.y = max.y; 
+
         return point;
 
     }
